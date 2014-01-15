04ac227a52e8415a9ed23dfcc7f14a7fa94e83f8
shelleybabij <babijs2@hotmail.com>
2014-01-15 14:20:15 -0500

    Assignment four for our responsive pool website
    Pool website assignment - responsive

---
+++ js/nav.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+// Toggle JavaScript
+
+	var navTop = document.querySelector('.nav-top');
+	
+	document.querySelector('.nav-btn').addEventListener('click', function(e){
+	e.preventDefault();	
+		
+	
+	if(navTop.getAttribute('data-state') == 'expanded'){
+		navTop.setAttribute('data-state', 'collapsed');
+	}else{
+		navTop.setAttribute('data-state', 'expanded');
+	}
+	
+	});
